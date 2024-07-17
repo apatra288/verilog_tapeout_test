4863d2d5ede2a4d2f1dc94c54cd8c39541f7f104
apatra288 <161279816+apatra288@users.noreply.github.com>
2024-07-16 21:56:55 -0700

    Update and rename project.v to tt_um_FIFO.v

---
+++ src/tt_um_FIFO.v
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2024 Your Name
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module tt_um_FIFO #(parameter WIDTH = 1, DEPTH = 32)(
+    input [WIDTH-1:0] data_in,
+    input read_en,
+    input write_en,
+    input clk,
+    input reset,
+    output reg [WIDTH-1:0] data_out,
+    output reg full,
+    output reg empty,
+    output reg [6:0] read_ptr,
+    output reg [6:0] write_ptr
+);
+
+reg [WIDTH-1:0] fifo[0:DEPTH-1];
+
+always @(posedge clk) begin
+    if (reset == 1'b1) begin
+        read_ptr <= 7'b0;
+        write_ptr <= 7'b0;
+        full <= 1'b0;
+        empty <= 1'b1;
+    end else begin
+        if (read_en == 1'b1 && empty == 1'b0) begin
+            data_out <= fifo[read_ptr];
+            read_ptr <= read_ptr + 1;
+            if (read_ptr == write_ptr) begin
+                empty <= 1'b1;
+            end
+        end
+
+        if (write_en == 1'b1 && full == 1'b0) begin
+            fifo[write_ptr] <= data_in;
+            write_ptr <= write_ptr + 1;
+            if ((read_ptr[6] != write_ptr[6]) && (read_ptr[5:0] == write_ptr[5:0])) begin
+                full <= 1'b1;
+            end
+            if (empty == 1'b1) begin
+                empty <= 1'b0;
+            end
+        end
+    end
+end
+
+endmodule
